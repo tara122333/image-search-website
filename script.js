e5fb8c1cf1023f0aff6112243d095df8157908ca
TARA CHAND KUMAWAT <tarachand122333@gmail.com>
2024-03-21 19:37:52 +0530

    Added search bar

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Image Search website');
+const inputSeachStringVal = document.getElementById("image-search-input").value;
+const searchForm = document.getElementById("search-form");
+const searchBtn = document.getElementById("search-btn");
+
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("Hii");
+});
