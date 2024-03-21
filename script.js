7b56b0c103634dd21b77e4174a8dbe39ae7fc6a4
TARA CHAND KUMAWAT <tarachand122333@gmail.com>
2024-03-21 20:14:59 +0530

    fetched images

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const inputSeachStringVal = document.getElementById("image-search-input").value;
+const accessToken = "QMmb4OjhUyXY-wcWNoMtg4jxFMsd3prqY01EDUXYc48";
+
+const inputSeach = document.getElementById("image-search-input");
 const searchForm = document.getElementById("search-form");
 const searchBtn = document.getElementById("search-btn");
 
+let page = 1;
+
+async function fetchImages() {
+  const inputSearchString = inputSeach.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputSearchString}&client_id=${accessToken}`;
+  const response = await fetch(url);
+  const data = await response.json();
+}
 
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log("Hii");
+  fetchImages();
 });
